a6099a30c9d4c98fa5b07f6572667250c16b4667
Youtpout <EddyBoughioul@hotmail.fr>
2024-04-30 18:25:31 +0200

    add agent

--- packages/chain/src/challenge.ts
+++ packages/chain/src/challenge.ts
@@ -1,7 +1,8 @@
-import { runtimeModule, state, runtimeMethod } from "@proto-kit/module";
-import { State, assert } from "@proto-kit/protocol";
+import { runtimeModule, state, runtimeMethod, RuntimeModule } from "@proto-kit/module";
+import { State, StateMap, assert } from "@proto-kit/protocol";
 import { Balance, Balances as BaseBalances, TokenId, UInt64 } from "@proto-kit/library";
-import { Field, PublicKey } from "o1js";
+import { Bool, Field, Poseidon, PublicKey, Struct } from "o1js";
+import { emptyValue } from "o1js/dist/node/lib/proof_system";
 
 interface Message {
     MessageNumber: Field;
@@ -14,25 +15,34 @@ interface MessageDetail {
     SecurityCode: [2];
 }
 
+
+export class AgentState extends Struct({
+    LastMessage: Field,
+    SecurityCode: Field,
+}) {
+
+    empty() {
+        return new AgentState({ LastMessage: Field(0), SecurityCode: Field(0) });
+    }
+
+    isEmpty() {
+        return Bool(this.LastMessage == Field(0) && this.SecurityCode == Field(0));
+    }
+}
+
 @runtimeModule()
-export class Challenge extends BaseBalances<Message> {
-    @state() public circulatingSupply = State.from<Balance>(Balance);
+export class Challenge extends RuntimeModule<Message> {
+    @state() public agentState = StateMap.from<Field, AgentState>(Field, AgentState);
 
     @runtimeMethod()
-    public addBalance(
-        tokenId: TokenId,
-        address: PublicKey,
-        amount: Balance
+    public addAgent(
+        AgentId: Field,
+        SecurityCode: Field[]
     ): void {
-        const circulatingSupply = this.circulatingSupply.get();
-        const newCirculatingSupply = Balance.from(circulatingSupply.value).add(
-            amount
-        );
-        assert(
-            newCirculatingSupply.lessThanOrEqual(this.config.totalSupply),
-            "Circulating supply would be higher than total supply"
-        );
-        this.circulatingSupply.set(newCirculatingSupply);
-        this.mint(tokenId, address, amount);
+        const agentStateCurrent = this.agentState.get(AgentId);
+        assert(agentStateCurrent.value.isEmpty(), "Agent already Exist");
+        assert(Bool(SecurityCode.length == 2), "Incorrect security code");
+        const securityCodeHash = Poseidon.hash(SecurityCode);
+        this.agentState.set(AgentId, new AgentState({ LastMessage: Field(0), SecurityCode: securityCodeHash }));
     }
 }
