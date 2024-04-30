f2c5e6e21b528b3ec534ae9e223808b3f974d554
youtpout <eddyboughioul@hotmail.fr>
2024-05-01 01:14:42 +0200

    cover more scenario

--- packages/chain/test/challenge.test.ts
+++ packages/chain/test/challenge.test.ts
@@ -35,11 +35,6 @@ describe("challenge", () => {
         appChain.setSigner(alicePrivateKey);
 
         contract = appChain.runtime.resolve("Challenge");
-    });
-
-
-
-    it("add new message", async () => {
 
         // add agent
         const tx1 = await appChain.transaction(alice, () => {
@@ -50,6 +45,9 @@ describe("challenge", () => {
 
 
         let block = await appChain.produceBlock();
+    });
+
+    it("add new message", async () => {
 
         let agent = await appChain.query.runtime.Challenge.agentState.get(Field(1));
         console.log("agent last message", agent?.LastMessage.toJSON());
@@ -70,9 +68,97 @@ describe("challenge", () => {
         await tx2.sign();
         await tx2.send();
 
-        block = await appChain.produceBlock();
+        await appChain.produceBlock();
         agent = await appChain.query.runtime.Challenge.agentState.get(Field(1));
         console.log("agent last message", agent?.LastMessage.toJSON());
+
+        // You should update the agent state to store the last message number received.
         expect(agent?.LastMessage).toEqual(Field(1));
     });
+
+
+    it("exist in system", async () => {
+        let message: Message = {
+            MessageNumber: Field(5),
+            MessageDetail: {
+                AgentId: Field(0),
+                SecurityCode: CircuitString.fromString("A5"),
+                Message: CircuitString.fromString("test12345678")
+            }
+        };
+        const tx2 = await appChain.transaction(alice, () => {
+            contract.addMessage(message);
+        });
+
+        await tx2.sign();
+        await tx2.send();
+
+        const block = await appChain.produceBlock();
+        // The AgentID exists in the system
+        expect(block?.transactions[0].statusMessage).toEqual("Agent didn't exist");
+    });
+
+    it("security code", async () => {
+        let message: Message = {
+            MessageNumber: Field(5),
+            MessageDetail: {
+                AgentId: Field(1),
+                SecurityCode: CircuitString.fromString("ZZ"),
+                Message: CircuitString.fromString("test12345678")
+            }
+        };
+        const tx2 = await appChain.transaction(alice, () => {
+            contract.addMessage(message);
+        });
+
+        await tx2.sign();
+        await tx2.send();
+
+        const block = await appChain.produceBlock();
+        // The security code matches that held for that AgentID
+        expect(block?.transactions[0].statusMessage).toEqual("Incorrect security code");
+    });
+
+    it("incorrect length", async () => {
+        let message: Message = {
+            MessageNumber: Field(5),
+            MessageDetail: {
+                AgentId: Field(1),
+                SecurityCode: CircuitString.fromString("A5"),
+                Message: CircuitString.fromString("small")
+            }
+        };
+
+        // The message is of the correct length.
+        await expect(async () => {
+            await appChain.transaction(alice, () => {
+                contract.addMessage(message);
+            })
+        }).rejects.toThrow("Incorrect length");
+
+    });
+
+    // it("Message number greather", async () => {
+
+    //     let agent = await appChain.query.runtime.Challenge.agentState.get(Field(1));
+    //     console.log("agent last message", agent?.LastMessage.toJSON());
+    //     expect(agent?.LastMessage).toEqual(Field(0));
+
+    //     let message: Message = {
+    //         MessageNumber: Field(0),
+    //         MessageDetail: {
+    //             AgentId: Field(1),
+    //             SecurityCode: CircuitString.fromString("A5"),
+    //             Message: CircuitString.fromString("test12345678")
+    //         }
+    //     };
+
+    //     // The message number is greater than the highest so far for that agent   
+    //     await expect(async () => {
+    //         await appChain.transaction(alice, () => {
+    //             contract.addMessage(message);
+    //         })
+    //     }).rejects.toThrow("Incorrect message number");
+
+    // });
 });
